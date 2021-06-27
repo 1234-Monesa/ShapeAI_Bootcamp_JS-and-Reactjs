a6c5b1bf5da5456c387c4ca8f11e8526d301f087
1234-Monesa <86568379+1234-Monesa@users.noreply.github.com>
2021-06-27 23:21:48 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p1> A basic web dev React Js Bootcamp</p1>
+      <p2> This was an amazing bootcamp we learned so much about js,HTML and react js</p2>
+      <p3> Thank you so much </p3>
+      </div>
+  );
+}
+
+
+
+export default Info;
